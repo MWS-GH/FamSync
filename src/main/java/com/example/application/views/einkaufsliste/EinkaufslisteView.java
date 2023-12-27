0ad42d0dfa23361e3dd7e33638b31a084c9dbc0f
start.vaadin.com <no-reply@vaadin.com>
2023-12-27 15:18:39 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/einkaufsliste/EinkaufslisteView.java
@@ -0,0 +1,36 @@
+package com.example.application.views.einkaufsliste;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("Einkaufsliste")
+@Route(value = "my-view", layout = MainLayout.class)
+@AnonymousAllowed
+@Uses(Icon.class)
+public class EinkaufslisteView extends Composite<VerticalLayout> {
+
+    public EinkaufslisteView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutColumn3.setWidth("100%");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutRow.add(layoutColumn3);
+    }
+}
